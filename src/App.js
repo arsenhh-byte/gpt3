7b8b7b0b39ad4acd6133d191fadf98722bad3968
Arsen Hendricks <ctrl-air@Arsens-MacBook-Air.local>
2023-05-15 12:41:28 +0300

    errors fixed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+//run rafce
 
-function App() {
+import React from 'react'
+import './App.css'
+
+import { Footer, Blog, Possibility, Features, WhatGPT4, Header } from './containers';
+import { CTA, Brand, Navbar} from './components';
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='gradient__bg'>
+        
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT4 />
+      <Features />
+      <Possibility />
+      <Blog />
+      <CTA />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/containers/whatGPT4/WhatGPT4.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './whatGPT4.css'
+
+const WhatGPT4 = () => {
+  return (
+    <div>WhatGPT4</div>
+  )
+}
+
+export default WhatGPT4
